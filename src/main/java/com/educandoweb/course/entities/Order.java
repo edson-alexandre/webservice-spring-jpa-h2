3c47975f6dafed49d84bba8f48597c197ecb72b4
Edson Alexandre <edson.alexandre@outlook.com>
2019-10-08 21:28:03 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -99,6 +99,16 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		Double total = 0.;
+		
+		for (OrderItem i : items) {
+			total += i.getSubTotal();
+		}
+		
+		return total;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -7,10 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_payment")
@@ -25,6 +26,7 @@ public class Payment implements Serializable {
 	
 	@OneToOne
 	@MapsId
+	@JsonIgnore
 	private Order order;
 	
 	public Payment() {		
