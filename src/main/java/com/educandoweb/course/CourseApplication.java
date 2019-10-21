7b90e34a5c866c69d250955253bb69f73ac89e97
Edson Alexandre <edson.alexandre@outlook.com>
2019-10-21 10:41:57 -0300

    PUT, DELETE E GET para Cliente

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,13 +1,36 @@
 package com.educandoweb.course;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.educandoweb.course.entities.Order;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);		
+		SpringApplication.run(CourseApplication.class, args);	
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Query q = em.createNativeQuery("SELECT * FROM tb_order");
+		List<Object[]> list = new ArrayList<>();
+		list = q.getResultList();
+		
+		for(Object[] o : list) {
+			System.out.println("Id: " + o[0] + " Moment: "+o[1] + "Status: " + o[2] + " Cliente: " +o[3]);
+		}
+		em.close();
+		emf.close();
 	}
 
 }
